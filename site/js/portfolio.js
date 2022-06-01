9fd3a2fdb8e20619f7bedce064067563645959e0
loulou8011984D <louptaxi@gmail.com>
2022-06-01 15:13:27 +0200

    ça avance lentement

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,14 @@
+//change avatar
 const avatarChange = document.querySelector('.avatar');
     avatarChange.addEventListener('click', function () {
     avatarChange.src = "image/avatar-bis.png";
     avatarChange.alt = "avatar2";
     });
-    
+//change name
+const userName = prompt("What's your name?");
+const changeName = document.querySelector('#name');
+changeName.innerHTML = `${userName}`;
+changeName.style.color = 'white';
+userName.onsubmit = function (event){
+    event.preventDefault();
+};
