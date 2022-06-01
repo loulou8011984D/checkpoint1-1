1af7dd7043f620e30b07f98185f368ab89e8b61b
loulou8011984D <louptaxi@gmail.com>
2022-06-01 12:32:41 +0200

    commit matinée

---
+++ site/js/portfolio.js
@@ -0,0 +1,6 @@
+const avatarChange = document.querySelector('.avatar');
+    avatarChange.addEventListener('click', function () {
+    avatarChange.src = "image/avatar-bis.png";
+    avatarChange.alt = "avatar2";
+    });
+    
