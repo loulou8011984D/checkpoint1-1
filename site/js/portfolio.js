7b82e0b6f92186acb9e8f89bb29190d661997d3a
loulou8011984D <louptaxi@gmail.com>
2022-06-01 22:35:38 +0200

    dernier de la soirée

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,12 +3,25 @@ const avatarChange = document.querySelector('.avatar');
     avatarChange.addEventListener('click', function () {
     avatarChange.src = "image/avatar-bis.png";
     avatarChange.alt = "avatar2";
-    });
-//change name
+});
+//change color to #750ff7
+const newColor1 = prompt("Enter a Color :");
 const userName = prompt("What's your name?");
+
+const newColor2 = document.querySelector('.pink-text','.description pink-bg');
+//const newColor3 = document.querySelector('.description');
+newColor2.style.color = '#750ff7';
+newColor2.style.backgroundColor = '#750ff7';
+
+
+//change name and color
 const changeName = document.querySelector('#name');
+const changeColor = document.querySelector('.pink-bg');
 changeName.innerHTML = `${userName}`;
 changeName.style.color = 'white';
+changeColor.style.backgroundColor = '#750ff7';
 userName.onsubmit = function (event){
     event.preventDefault();
 };
+
+
